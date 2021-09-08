e8c52256bd4a1cde61e0d17a35929c8ff9757083
lapaduga <47709041+lapaduga@users.noreply.github.com>
2021-09-08 12:09:11 +0500

    header done

--- js/script.js
+++ js/script.js
@@ -28,26 +28,50 @@ const isMobile = {
 
 if (isMobile.any()) {
 	document.body.classList.add('_touch');
-	
+
 	let menuArrows = document.querySelectorAll('.menu__arrow');
 	if (menuArrows.length > 0) {
 		for (let i = 0; i < menuArrows.length; i++) {
 			const menuArrow = menuArrows[i];
-			menuArrow.addEventListener('click', function(e) {
+			menuArrow.addEventListener('click', function (e) {
 				menuArrow.parentElement.classList.toggle('_active');
 			});
 		}
 	}
-} else{
+} else {
 	document.body.classList.add('_pc');
 }
 
+
+// Burger menu
 const burger = document.querySelector('.header__icon');
 if (burger) {
 	const menu = document.querySelector('.menu');
-	burger.addEventListener('click', function(e) {
+	burger.addEventListener('click', function (e) {
 		document.body.classList.toggle('_lock');
 		burger.classList.toggle('_active');
 		menu.classList.toggle('_active');
 	});
 }
+
+// Header behaviour on scroll
+let lastScroll = 10;
+window.addEventListener('scroll', () => {
+	const currentScroll = window.pageYOffset;
+
+	if (currentScroll <= 0) {
+		document.body.classList.remove('scroll-up');
+	}
+
+	if (currentScroll > lastScroll && !document.body.classList.contains('scroll-down')) {
+		document.body.classList.remove('scroll-up');
+		document.body.classList.add('scroll-down');
+	}
+
+	if (currentScroll < lastScroll && document.body.classList.contains('scroll-down')) {
+		document.body.classList.remove('scroll-down');
+		document.body.classList.add('scroll-up');
+	}
+
+	lastScroll = currentScroll;
+});
